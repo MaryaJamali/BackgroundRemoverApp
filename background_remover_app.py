8487c4afd8e7b81efee058e3834434e2895f13d2
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-06-21 18:18:07 +0330

    Coding the main body

--- background_remover_app.py
+++ background_remover_app.py
@@ -8,3 +8,12 @@ import os  # Import the os library to work with the file system
 class BackgroundRemoverApp:
     def __init__(self, root):
         self.root = root
+        self.root.title("Remove image background")  # Set the title of the main window
+        self.root.geometry("600x400")  # Set window size
+        self.root.configure(background='white')  # Set the background color of the window to white
+
+        self.input_image_path = None  # Stores the input image path
+        self.output_image_path = None  # Stores the output image path
+
+        self.mode_var = StringVar()  # Create a StringVar variable to store the selected mode (color or black and white)
+        self.mode_var.set("color")  # Set the default mode to color
